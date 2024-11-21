520b728143881613052de3a53d9e3862b636caa8
Saiteja93 <saitejaraj.guvvala@gmail.com>
2024-11-20 16:43:43 -0800

    variables

---
+++ 06-Arrays.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+fruits=("kiwi" "apple" "banana")
+
+echo "First fruit is: ${fruits[1]}"
+echo "Second fruit is: ${fruits[2]}"
+echo "Third fruit is: ${fruits[3]}"
+echo "first fruit is: ${fruits[@]}"

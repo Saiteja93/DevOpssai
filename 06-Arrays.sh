82ab44a716bda8375f7a0e3512656dfb127f2fc3
Saiteja93 <saitejaraj.guvvala@gmail.com>
2024-11-20 16:45:44 -0800

    new file

--- 06-Arrays.sh
+++ 06-Arrays.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 fruits=("kiwi" "apple" "banana")
 
-echo "First fruit is: ${fruits[1]}"
-echo "Second fruit is: ${fruits[2]}"
-echo "Third fruit is: ${fruits[3]}"
+echo "First fruit is: ${fruits[0]}"
+echo "Second fruit is: ${fruits[1]}"
+echo "Third fruit is: ${fruits[2]}"
 echo "first fruit is: ${fruits[@]}"
